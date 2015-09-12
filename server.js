8bc02ea129b011a395b44010e8be33edd4a99ce1
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-09-12 18:52:09 +0000

    Azure

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+
+//
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*
 var GCM = require('./gcm.js');
 var gcm = new GCM('AIzaSyB7uOYVF4PBiNz1DRoA3k_YXHrlGsYnPuQ');
 var express = require('express');
@@ -78,6 +110,8 @@ function broadcast(event, data) {
 }
 */
 
+
+/*
 app.post('/push', function (req, res) {
       
       var rid = req.body.rid;
@@ -106,3 +140,4 @@ app.post('/push', function (req, res) {
 server.listen(process.env.PORT, function(){
   
 });
+*/
