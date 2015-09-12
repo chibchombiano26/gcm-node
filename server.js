d117c4b8ae3eaa76e5e914f18b521549c0607dc3
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-09-12 18:18:10 +0000

    Azure

--- server.js
+++ server.js
@@ -3,17 +3,77 @@ var gcm = new GCM('AIzaSyB7uOYVF4PBiNz1DRoA3k_YXHrlGsYnPuQ');
 var express = require('express');
 var app = express();
 var http = require('http');
+var path = require('path');
+var async = require('async');
+var socketio = require('socket.io');
 
 
-app.set('port', process.env.PORT || 3000);
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
+
+var server = http.createServer(app);
+var io = socketio.listen(server);
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+var messages = [];
+var sockets = [];
+
+io.on('connection', function (socket) {
+    messages.forEach(function (data) {
+      socket.emit('message', data);
+    });
+
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+      updateRoster();
+    });
+
+    socket.on('message', function (msg) {
+      var text = String(msg || '');
+
+      if (!text)
+        return;
+
+      socket.get('name', function (err, name) {
+        var data = {
+          name: name,
+          text: text
+        };
+
+        broadcast('message', data);
+        messages.push(data);
+      });
+    });
+
+    socket.on('identify', function (name) {
+      socket.set('name', String(name || 'Anonymous'), function (err) {
+        updateRoster();
+      });
+    });
+  });
+
+function updateRoster() {
+  async.map(
+    sockets,
+    function (socket, callback) {
+      socket.get('name', callback);
+    },
+    function (err, names) {
+      broadcast('roster', names);
+    }
+  );
+}
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
 
 app.post('/push', function (req, res) {
       
@@ -40,7 +100,7 @@ app.post('/push', function (req, res) {
 });
 
 
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Servidor andando en ", app.get('port'));
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
