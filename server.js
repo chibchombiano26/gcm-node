737a2d2221d9768fd3596100afa9a255edb794fa
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-09-13 18:14:56 +0000

    13 de septiembre

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-
 var express = require('express');
 var app = express();
 var http = require('http');
@@ -7,21 +6,15 @@ var path = require('path');
 var notificaciones = require('./notificaciones/notification.js');
 var realtime = require("./realtime/socketIo.js");
 var cors = require("./midelware/cors.js");
-
-
-
 var socketio = require('socket.io');
-
-
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-app.use(cors.headers);
-
 var server = http.createServer(app);
 var io = socketio.listen(server);
 
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors.headers);
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 
@@ -40,5 +33,5 @@ app.post('/notifications', function (req, res) {
 
 
 server.listen(process.env.PORT, function(){
-  
+  console.log("Servidor andando en puerto", process.env.PORT);
 });
